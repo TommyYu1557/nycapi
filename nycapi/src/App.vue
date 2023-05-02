5b35c118582092e2ae7cf686f9556e9a0dada158
Tommy Yu <ytommy0111@aol.com>
2023-05-02 18:42:53 -0400

    wipe

--- nycapi/src/App.vue
+++ nycapi/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '../components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -83,4 +83,3 @@ nav a:first-of-type {
   }
 }
 </style>
-<!-- something something were going to graph the NYPD arrests data and show a pie chart of the TYPEs of arrests.  -->
