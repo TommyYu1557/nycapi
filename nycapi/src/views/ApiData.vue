fdfbdd7d3222687ff78e00a9bbe2f57a51549e13
Tommy Yu <ytommy0111@aol.com>
2023-05-02 20:22:53 -0400

    saved

---
+++ nycapi/src/views/ApiData.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import CrimeCards from './components/CrimeCards.vue'
+
+const crime = ref('')
+async function getCrime() {
+  const response = await fetch('https://data.cityofnewyorhttps://data.cityofnewyork.us/resource/uip8-fykc.jsonk.us/https://data.cityofnewyork.us/resource/qb7u-rbmr.json/ia2d-e54m.json')
+  const data = await response.json()
+  crime.value = data
+  console.log(data);
+}
+onMounted(() => {
+  getCrime()
+})
+</script>
+
+<template>
+  <div class="container">
+    <CrimeCards v-for="(title, index) in crime" :key="title.date" :id="index + 1" :crime="title" />
+  </div>
+</template>
