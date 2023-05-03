e7ecb6d0c75be391eaab67e488d6ff44e8f5bece
Tommy Yu <ytommy0111@aol.com>
2023-05-02 20:20:36 -0400

    i think something is working

--- nycapi/src/router/index.js
+++ nycapi/src/router/index.js
@@ -1,21 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import ApiData from '../views/ApiData.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'ApiData',
+      component: ApiData
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/ApiData',
+      name: 'ApiData',
+      component: () => import('../views/ApiData.vue')
+    },
+
+    {
+      path: '/pieView',
+      name: 'pieView',
+      component: () => import('../views/pieView.vue')
+    },
+    {
+      path: '/barView',
+      name: 'barView',
+      component: () => import('../views/barView.vue')
     }
   ]
 })
